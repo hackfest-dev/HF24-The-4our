111116fffc97225ccbc52ba6f8d208e9f48c574c
shrey-113 <shreyanshngp11@gmail.com>
2024-04-06 18:33:41 +0530

    farms fetching and investor data controller done

--- client/src/components/AdminFarms.jsx
+++ client/src/components/AdminFarms.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function AdminFarms() {
-  return (
-    <div>AdminFarms</div>
-  )
-}
+const AdminFarms = () => {
+  const [farmsData, setFarmsData] = useState([]);
+  const apiurl = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    const fetchFarms = async () => {
+      try {
+        const response = await fetch(`${apiurl}/fetch/getallfarms`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch farms");
+        }
+        const data = await response.json();
+        setFarmsData(data);
+        console.log(data); // Log the fetched data to the console
+      } catch (error) {
+        console.error("Error fetching farms:", error);
+      }
+    };
+
+    fetchFarms();
+  }, []);
+
+  return <div></div>;
+};
+
+export default AdminFarms;
