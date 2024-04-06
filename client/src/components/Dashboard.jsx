230a754e7a59fdd2522eb467b8fed20b47d8fa92
r4tnx <ratneshkherudkar@gmail.com>
2024-04-07 00:35:46 +0530

    add news

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import hero from '../images/hero.png'
 
 export default function Dashboard() {
   return (
-    <div>
-        Dashboard
+    <div className='flex w-screen h-screen justify-end items-center'>
+      <img className='h-[500px]' src={hero} alt="" />
     </div>
   )
 }
